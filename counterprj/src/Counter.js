73916757183e1d832424be191a201fc6b1d2f5a4
Atul-me <atulkur1999@gmail.com>
2024-03-21 13:17:13 +0530

    re1

--- counterprj/src/Counter.js
+++ counterprj/src/Counter.js
@@ -2,20 +2,18 @@ import React, { useState } from 'react';
 import './style.css';
 
 function Counter() {
-    const [count, setcount] = useState(0);
-    const increment = () => setcount(count+1);
-    const decrement = () => setcount(count-1);
+  const [count, setcount] = useState(0);
+  const increment = () => setcount(count+1);
+  const decrement = () => setcount(count-1);
+
   return (
     <>
-    <div className='container'>
-        <h1 className='number'>{count}</h1>
+    <div className="container">
+      <h1 className='number'>{count}</h1>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
     </div>
-    <button onClick={increment}>
-        +1
-    </button>
-    <button onClick={decrement}>
-        -1
-    </button>
+    
     </>
   )
 }
