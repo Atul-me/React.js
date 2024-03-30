8919059a19d83b32fdb5c4e99282cff196508931
Atul-me <atulkur1999@gmail.com>
2024-03-30 18:00:43 +0530

    re8

---
+++ React/src/components/Body.js
@@ -0,0 +1,15 @@
+
+
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                search
+            </div>
+            <Rescard/>
+        </div>
+    )
+}
+
+export default Body;
---
+++ React/src/components/Rescard.js
@@ -0,0 +1,11 @@
+
+
+const Rescard = () => {
+    return (
+        <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+            
+        </div>
+    )
+}
+
+export default Rescard;
