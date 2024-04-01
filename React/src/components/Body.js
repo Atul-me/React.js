5143e3a4f0fd2e7d5110b5afd2382be4af32fed3
Atul-me <atulkur1999@gmail.com>
2024-04-01 12:03:49 +0530

    re9

--- React/src/components/Body.js
+++ React/src/components/Body.js
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ResData } from "../utils/ResData";
 import Rescard from "./Rescard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listRes, setListRes] = useState([ResData]);
-  return (
+  const [listRes, setListRes] = useState([]);
+
+  useEffect(()=>{
+    fetchData();
+  },[]);
+
+  const fetchData = async() => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+
+    const json = await data.json();
+    setListRes(json?.data?.cards[2]?.data?.data?.cards);
+  };
+  return listRes.length === 0 ? <Shimmer /> :  (
     <div className="body">
       {/* <div className="search">
                 search
---
+++ React/src/components/Shimmer.js
@@ -0,0 +1,18 @@
+const Shimmer = () => {
+    return (
+        <div className="container">
+            <div className="shimmer"></div>
+            <div className="shimmer"></div>
+            <div className="shimmer"></div>
+            <div className="shimmer"></div>
+            <div className="shimmer"></div>
+            <div className="shimmer"></div>
+            <div className="shimmer"></div>
+            <div className="shimmer"></div>
+            <div className="shimmer"></div>
+            <div className="shimmer"></div>
+        </div>
+    )
+}
+
+export default Shimmer;
